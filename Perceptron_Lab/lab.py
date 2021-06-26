89117f44c4c460fceabae769e8927e869b8d18e1
kavindu <kavindukusal@gmail.com>
2021-06-26 18:26:57 -0400

    shuffling works

--- Perceptron_Lab/lab.py
+++ Perceptron_Lab/lab.py
@@ -7,63 +7,34 @@ import matplotlib.pyplot as plt
 class PerceptronClassifier(BaseEstimator,ClassifierMixin):
 
     def __init__(self, lr=.1, shuffle=True, epochs=None):
-        """ 
-            Initialize class with chosen hyperparameters.
-        Args:
-            lr (float): A learning rate / step size.
-            shuffle: Whether to shuffle the training data each epoch. DO NOT 
-            SHUFFLE for evaluation / debug datasets.
-        """
         self.lr = lr
         self.shuffle = shuffle
         self.epochs = epochs
+        self.accuracy = 0
 
     def fit(self, X: np.ndarray, y: np.ndarray, initial_weights=None):
-        """ 
-            Fit the data; run the algorithm and adjust the weights to find a 
-            good solution
-        Args:
-            X (array-like): A 2D numpy array with the training data, excluding
-            targets
-            y (array-like): A 2D numpy array with the training targets
-            initial_weights (array-like): allows the user to provide initial 
-            weights
-        Returns:
-            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
-        """
-
         self.weights = self.initialize_weights(X.shape[1]) if not initial_weights else initial_weights
         self.X = X
         self.y = y
 
-        # bias = np.ones(X.shape[0]).reshape(X.shape[0],1)
-        # X = np.append(X, bias, axis=1)
-
-        # net = None
-        # output = None
-
-        # for i in range(X.shape[0]):        
-        #     net = np.dot(X[i],self.weights)
-        #     output = 1 if net > 0 else 0
-        #     dWeight = self.lr*(y[i] - output)*X[i]
-        #     self.weights = self.weights + dWeight    
+        bias = np.ones(X.shape[0]).reshape(X.shape[0],1)
+        X = np.append(X, bias, axis=1)
+   
         if self.epoch == None:
-            pass
+            i=0
+            accuracy = 2
+            while np.mod(accuracy-self.accuracy) > 1 or i <=50:
+                accuracy = self.accuracy
+                self.epoch(X)
+                self.accuracy = self.score(self.X, self.y)
+                i = i + 1
         else:
             for i in range(self.epochs):
-                self.epoch()         
+                self.epoch(X)
+                # print(self.score(self.X, self.y))         
         return self
 
     def predict(self, X:np.ndarray):
-        """ 
-            Predict all classes for a dataset X
-        Args:
-            X (array-like): A 2D numpy array with the training data, excluding 
-            targets
-        Returns:
-            array, shape (n_samples,)
-                Predicted target values per element in X.
-        """
         net  = X @ self.weights
         net = net.to_list()
         yHat = [1 if item>0 else 0 for item in net]
@@ -71,23 +42,10 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
  
 
     def initialize_weights(self, n):
-        """ Initialize weights for perceptron. Don't forget the bias!
-        Returns:
-        """
         weights = np.zeros((n+1))
         return weights
 
     def score(self, X, y):
-        """ 
-            Return accuracy of model on a given dataset. Must implement own 
-            score function.
-        Args:
-            X (array-like): A 2D numpy array with data, excluding targets
-            y (array-like): A 2D numpy array with targets
-        Returns:
-            score : float
-                Mean accuracy of self.predict(X) wrt. y.
-        """
         yHat = self.predict(X)
         counter = 0
         for yy, yh in zip(y, yHat):
@@ -96,27 +54,23 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
         return counter*100/len(y)
 
     def _shuffle_data(self, X, y):
-        """ 
-            Shuffle the data! This _ prefix suggests that this method should 
-            only be called internally.
-            It might be easier to concatenate X & y and shuffle a single 2D 
-            array, rather than shuffling X and y exactly the same way, 
-            independently.
-        """
-        concat = np.append(X,y, axis=1)
+        concat = np.append(X,y.reshape(len(y),1), axis=1)
         np.random.shuffle(concat)
         return concat[:, :-1], concat[:,-1]
 
-    def epoch(self):
-        bias = np.ones(self.X.shape[0]).reshape(self.X.shape[0],1)
-        X = np.append(self.X, bias, axis=1)
+    def epoch(self,X):
+        if self.shuffle == True:
+            X, y = self._shuffle_data(X,self.y)
+        else:
+            y = self.y
+
         net = None
         output = None
 
         for i in range(X.shape[0]):        
             net = np.dot(X[i],self.weights)
             output = 1 if net > 0 else 0
-            dWeight = self.lr*(self.y[i] - output)*X[i]
+            dWeight = self.lr*(y[i] - output)*X[i]
             self.weights = self.weights + dWeight
         print(self.weights)        
 
