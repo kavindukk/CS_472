43841ad523c377694cc190698b314ce9905d935c
kavindu <kavindukusal@gmail.com>
2021-06-26 23:14:53 -0400

    Q1 completed

--- Perceptron_Lab/lab.py
+++ Perceptron_Lab/lab.py
@@ -13,6 +13,7 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
         self.lr = lr
         self.shuffle = shuffle
         self.epochs = epochs
+        self.accuracy=0
 
     def fit(self, X: np.ndarray, y: np.ndarray, initial_weights=None):
         self.weights = self.initialize_weights(X.shape[1]) if not initial_weights else initial_weights
@@ -26,14 +27,16 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
             i=0
 
             while self.accuracy <= .95 and i <=50:
+                # print("Epoch " + str(i+1))
                 self.epoch(X)
                 self.accuracy = self.score(X, self.y)
                 i = i + 1
-                print(self.score(X, self.y))  
+                # print("Accuracy: " + str(self.score(X, self.y)) + "\n")  
         else:
             for i in range(self.epochs):
+                # print("Epoch " + str(i+1))
                 self.epoch(X)
-                print(self.score(X, self.y))         
+                # print("Accuracy: " + str(self.score(X, self.y)) + "\n")           
         return self
 
     def predict(self, X:np.ndarray):
@@ -45,13 +48,17 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
     def initialize_weights(self, n):
         weights = np.zeros((n+1))
         return weights
-        
+
     def score(self, X, y):
+        bias = np.ones(X.shape[0]).reshape(X.shape[0],1)
+        X = np.append(X, bias, axis=1)
         yHat = self.predict(X)
         counter = 0
         for yy, yh in zip(y, yHat):
             if yy == yh:
                 counter = counter + 1
+        self.accuracy = counter/len(y)
+        self.print_data()
         return counter/len(y)
 
     def _shuffle_data(self, X, y):
@@ -73,13 +80,26 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
             output = 1 if net > 0 else 0
             dWeight = self.lr*(y[i] - output)*X[i]
             self.weights = self.weights + dWeight
-        print(self.weights)        
+        # print("Weights: " +np.array2string(self.weights, precision=2, separator=',',suppress_small=True ))
+
+    def create_test_data(self, X, y, percentage):
+        #Give the percentage as a value from 0-1; 10% eqals to 0.1
+        concat = np.append(X,y.reshape(len(y),1), axis=1)
+        noOfDataPoints = X.shape[0] * percentage
+        noOfDataPoints = int(np.rint(noOfDataPoints))
+        concat = concat[np.random.choice(concat.shape[0], noOfDataPoints, replace=False)]
+        return concat[:, :-1], concat[:,-1]
 
     ### Not required by sk-learn but required by us for grading. Returns the weights.
     def get_weights(self):
         return self.weights
 
-data = arff.loadarff("b.arff")
+    def print_data(self):
+        print("Weights: " +np.array2string(self.weights, precision=2, separator=',',suppress_small=True ))
+        print("Accuracy: " + str(self.accuracy) + "\n")
+
+
+data = arff.loadarff("linsep2nonorigin.arff")
 df = pd.DataFrame(data[0])
 X = df.iloc[:,:-1].to_numpy()
 Y = df.iloc[:,-1].to_numpy().astype(np.int)
@@ -98,5 +118,7 @@ Y = df.iloc[:,-1].to_numpy().astype(np.int)
 # ])
 # Y = np.array([1,1,1,1,0,0,0,0])
 
-A = PerceptronClassifier(lr=0.1, shuffle=False, epochs=None)
-A.fit(X=X, y=Y)
+A = PerceptronClassifier(lr=0.1, shuffle=False, epochs=10)
+xEval, yEval = A.create_test_data(X,Y,1)
+Accuracy = A.fit(X=X, y=Y).score(xEval,yEval)
+# aSelf.print_data(xEval,yEval)
