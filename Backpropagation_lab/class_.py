8d6f2a84c9a8c55edfca149a423c94b9e8baffd6
kavindu <kavindukusal@gmail.com>
2021-07-06 19:42:29 -0400

    class fully completed

--- Backpropagation_lab/class_.py
+++ Backpropagation_lab/class_.py
@@ -1,5 +1,6 @@
 from numpy.core.defchararray import count
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,17 +8,6 @@ import matplotlib.pyplot as plt
 class MLP(BaseEstimator,ClassifierMixin):
 
     def __init__(self,lr=.1, momentum=0, shuffle=True,hidden_layer_widths=None, bias=1):
-        """ Initialize class with chosen hyperparameters.
-
-        Args:
-            lr (float): A learning rate / step size.
-            shuffle(boolean): Whether to shuffle the training data each epoch. DO NOT SHUFFLE for evaluation / debug datasets.
-            momentum(float): The momentum coefficent 
-        Optional Args (Args we think will make your life easier):
-            hidden_layer_widths (list(int)): A list of integers which defines the width of each hidden layer if hidden layer is none do twice as many hidden nodes as input nodes.
-        Example:
-            mlp = MLP(lr=.2,momentum=.5,shuffle=False,hidden_layer_widths = [3,3]),  <--- this will create a model with two hidden layers, both 3 nodes wide
-        """
         self.hidden_layer_widths = hidden_layer_widths
         self.lr = lr
         self.momentum = momentum
@@ -26,42 +16,29 @@ class MLP(BaseEstimator,ClassifierMixin):
         self.DeltaW = None
 
 
-    def fit(self, X, y, initial_weights=None, epochs=10):
-        """ Fit the data; run the algorithm and adjust the weights to find a good solution
-
-        Args:
-            X (array-like): A 2D numpy array with the training data, excluding targets
-            y (array-like): A 2D numpy array with the training targets
-        Optional Args (Args we think will make your life easier):
-            initial_weights (array-like): allows the user to provide initial weights
-        Returns:
-            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
-
-        """
+    def fit(self, X, y, initial_weights=None, epochs=10, validation_percentage=.15):
         # self.weights = self.initialize_weights(X.shape[1],y.shape[1], initial_weights) if not initial_weights else initial_weights
         Xdim = self.get_dim(X)
         Ydim = self.get_dim(y)
         # Ydim = 2 if self.get_dim(y)==1 else self.get_dim(y)
         self.weights = self.initialize_weights(Xdim,Ydim, initial_weights) 
         self.initialize_delta_weights()
-        # self.X = X
-        # self.y = y
 
-        for i in range(epochs):
-            self.epoch(X,y)
+        if not epochs:            
+            xTrain, xValid, yTrain, yValid = self.split_data(X, y, percentage=validation_percentage)
+            accuracy = 0
+            accuracyRepeatCount = 0
+            while accuracy< 0.85:
+                self.epoch(xTrain,yTrain)
+                currentAccuracy = self.score(xValid,yValid)
+                if np.abs(accuracy-currentAccuracy) <= 0.02 : accuracyRepeatCount = accuracyRepeatCount + 1
+                accuracy = currentAccuracy
+        else:
+            for i in range(epochs):
+                self.epoch(X,y)
         return self
 
     def score(self, X, y):
-        """ Return accuracy of model on a given dataset. Must implement own score function.
-
-        Args:
-            X (array-like): A 2D numpy array with data, excluding targets
-            y (array-like): A 2D numpy array with targets
-
-        Returns:
-            score : float
-                Mean accuracy of self.predict(X) wrt. y.
-        """
         outputsOrIndexes = self.predict(X, y.ndim)
         accuracyCount = 0
         if y.ndim == 1:
@@ -112,11 +89,6 @@ class MLP(BaseEstimator,ClassifierMixin):
 
 
     def initialize_weights(self, noOfInputs, noOfOutputs, weights):
-        """ Initialize weights for perceptron. Don't forget the bias!
-
-        Returns:
-
-        """
         if self.hidden_layer_widths == None:
             self.network = [noOfInputs, 2*noOfInputs, noOfOutputs]
             weights = self.assign_weight_matrix(weights)
@@ -197,10 +169,6 @@ class MLP(BaseEstimator,ClassifierMixin):
         return deltaL[::-1]
 
     def _shuffle_data(self, X, y):
-        """ Shuffle the data! This _ prefix suggests that this method should only be called internally.
-            It might be easier to concatenate X & y and shuffle a single 2D array, rather than
-             shuffling X and y exactly the same way, independently.
-        """
         concat = np.append(X,y.reshape(len(y),1), axis=1)
         np.random.shuffle(concat)
         return concat[:, :-1], concat[:,-1]
@@ -211,6 +179,10 @@ class MLP(BaseEstimator,ClassifierMixin):
         else:
             dimX = X.shape[1]
         return dimX 
+
+    def split_data(self, X, y, percentage):
+        xTrain, xEval, yTrain, yEval = train_test_split(X,y,test_size=percentage)
+        return xTrain, xEval, yTrain, yEval
 
     ### Not required by sk-learn but required by us for grading. Returns the weights.
     def get_weights(self):
